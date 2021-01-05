d0f1292d1fa8dff34d655a9a4b259518a91aa506
inees24 <72389212+inees24@users.noreply.github.com>
2021-01-05 12:47:02 +0100

    debut

--- app/src/main/java/com/example/bts_sio/MainActivity.java
+++ app/src/main/java/com/example/bts_sio/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.bts_sio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView mGreetingText;
+        mGreetingText = (TextView) findViewById(R.id.activity_main_greeting_txt);
     }
 }
