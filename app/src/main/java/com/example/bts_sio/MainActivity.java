f3cbd8dff42f8b8b9ee72aa9e8e94e67483e061e
inees24 <72389212+inees24@users.noreply.github.com>
2021-01-05 13:45:58 +0100

    ajout menu

--- app/src/main/java/com/example/bts_sio/MainActivity.java
+++ app/src/main/java/com/example/bts_sio/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.bts_sio;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,4 +18,25 @@ public class MainActivity extends AppCompatActivity {
         TextView mGreetingText;
         mGreetingText = (TextView) findViewById(R.id.activity_main_greeting_txt);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater() .inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId() ) {
+            case R.id.menu_SLAM3:
+                Toast.makeText(this, "menu SLAM3 sélectionné", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.menu_SLAM4:
+                Toast.makeText(this, "menu SLAM4 sélectionné", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
